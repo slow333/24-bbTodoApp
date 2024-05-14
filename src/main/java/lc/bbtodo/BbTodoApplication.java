a07ab4e05ba95b6bfa4e0d0c0e60db901edea07e
self esteem <selfesteem@gmail.com>
2024-05-15 07:34:12 +0900

    first commit

---
+++ src/main/java/lc/bbtodo/BbTodoApplication.java
@@ -0,0 +1,13 @@
+package lc.bbtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbTodoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BbTodoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/lc/bbtodo/BbTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package lc.bbtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbTodoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
